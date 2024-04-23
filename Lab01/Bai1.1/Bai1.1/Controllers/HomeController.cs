7f3caf5d7fd56034786e47a0c1d7014882f54562
Nguyen-Thi-Tra-Mi-0304 <traminguyen0304@gmail.com>
2024-04-23 21:24:14 +0700

    Bai1.1

---
+++ Lab01/Bai1.1/Bai1.1/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bai1._1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Home/About 
+        public ActionResult About()
+        {
+            return View();
+        }
+        // GET: Home/Contact
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lab01/Bai1.1/Bai1.1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bai1._1.MvcApplication" Language="C#" %>
