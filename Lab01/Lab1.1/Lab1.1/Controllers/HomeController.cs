465033b32f6220fc8d825f831645989065074519
Nguyen-Thi-Tra-Mi-0304 <traminguyen0304@gmail.com>
2024-04-23 12:08:49 +0700

    Lab01

---
+++ Lab01/Lab1.1/Lab1.1/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1._1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //sử dung ViewBag để đưa dữ liệu ra View
+            ViewBag.message = "Chào mừng bạn đến với ASP.NET MVC 5";
+            return View();
+        }
+        public ActionResult About()
+        {
+            //sử dung ViewBag để đưa dữ liệu ra View
+            ViewBag.message = "Đây là trang About";
+            return View();
+        }
+    }
+}
---
+++ Lab01/Lab1.2/Lab1.2/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1._2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //GET: Home/About
+        public ActionResult About()
+        {
+            ViewBag.Message = "Research IT - DEVMASTER / Chúng tôi sẽ làm bạn hài lòng!";
+            return View();
+        }
+        //GET: Home/Contact
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Thông tin liên hệ";
+            return View();
+        }
+        //GET: Home/Products
+        public ActionResult Products()
+        {
+            ViewBag.Message = "Thực đơn hôm nay";
+            string[] products = { "Gà quay", "Cá chép om dưa", "Vịt quay vân đình", "Ốc xào xả ớt" };
+            ViewBag.products = products;
+            return View();
+        }
+    }
+}
