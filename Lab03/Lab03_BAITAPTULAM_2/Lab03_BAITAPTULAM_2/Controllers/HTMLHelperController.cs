f20f4272c5a673bd42ec9f89e4a5dd8ca84aa2b3
Nguyen-Thi-Tra-Mi-0304 <traminguyen0304@gmail.com>
2024-05-10 13:31:40 +0700

    Lab03_BAITAPTULAM_2

---
+++ Lab03/Lab03_BAITAPTULAM_2/Lab03_BAITAPTULAM_2/Controllers/HTMLHelperController.cs
@@ -0,0 +1,44 @@
+﻿using Lab03_BAITAPTULAM_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab03_BAITAPTULAM_2.Controllers
+{
+    public class HTMLHelperController : Controller
+    {
+        // GET: HTMLHelper
+        public ActionResult FormRegister()
+        {
+            // tạo list cho droplist
+            ViewBag.listCompany = new List<Company>()
+            {
+                new Company(){Id="0",Name="-- Chọn Công ty --"},
+                new Company(){Id="NN",Name="Đơn vị sự nghiệp nhà nước"},
+                new Company(){Id="TN",Name="Đơn vị sự nghiệp tư nhân"},
+                new Company(){Id="TNHH",Name="Công ty TNHH một thành viên"},
+                new Company(){Id="CP",Name="Công ty cổ phần"},
+                new Company(){Id="TNHH2",Name="Công ty TNHH từ 2 thành viên trở lên"}
+            };
+            return View();
+        }
+        public ActionResult Register() {
+            // lấy giá trị được  các trường đẩy lên server khi submit 
+            TempData["CName"] = Request["txtCName"];
+            TempData["Company"] = Request["txtCompany"];
+            TempData["Quantity"] = Request["txtQuantity"];
+            TempData["Address"] = Request["txtAddress"];
+            TempData["FName"] = Request["txtFName"];
+            TempData["Phone"] = Request["txtPhone"];
+            TempData["Fax"] = Request["txtFax"];
+            TempData["Email"] = Request["txtEmail"];
+            TempData["AEmail"] = Request["txtAEmail"];
+            TempData["UName"] = Request["txtUName"];
+            TempData["Pass"] = Request["txtPass"];
+            TempData["Mail"] = Request["Mail"].ToString();
+            return View();
+        }
+    }
+}
---
+++ Lab03/Lab03_BAITAPTULAM_2/Lab03_BAITAPTULAM_2/Models/Company.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab03_BAITAPTULAM_2.Models
+{
+    public class Company
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
